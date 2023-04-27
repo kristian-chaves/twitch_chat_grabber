9631be11ffd049238f4f193045dcce3737f2fe36
uw-square <68247804+uw-square@users.noreply.github.com>
2023-04-26 23:43:22 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const oAuth = "dy71ezmwmejfoyhx8pkntuya1yno2h";
+//const oAuth = "5pm2jgry9oqeghu1nunqe5pprrq3iz";
+
+const nick = "squaresyyy";
+const channel = "boxbox";
+const fs = require('fs')
+
+
+Object.assign(global, { WebSocket: require('ws') });
+const socket = new WebSocket('wss://irc-ws.chat.twitch.tv:443');
+
+socket.addEventListener('open', (event) => {
+    socket.send(`Pass oauth:${oAuth}`);
+    socket.send(`NICK ${nick}`)
+    socket.send(`JOIN #${channel}`)
+})  
+
+socket.addEventListener('message', event => {
+    //save just message data
+    if (event.data.includes("PRIVMSG")){
+        fs.appendFile('Output.txt', event.data, (err) => {          
+            // In case of a error throw err.
+            if (err) throw err;
+        })
+            //console.log(event.data);
+    }
+    if(event.data.includes("PING")) socket.send("PONG");
+})
+
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+Object.assign(global, { WebSocket: require('ws') });
